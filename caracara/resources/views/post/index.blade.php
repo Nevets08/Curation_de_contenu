aa07c36a49ab082651c60aaf9089c0fb63f98ef1
Johana <jojob.1234@sfr.fr>
2021-02-10 18:31:53 +0100

    Post Policy :D

--- caracara/resources/views/post/index.blade.php
+++ caracara/resources/views/post/index.blade.php
@@ -6,24 +6,26 @@
 </head>
 <body>
     @foreach ($posts as $post)
-        <div style="margin: 50px">
-            <a href="{{ $post->url }}" target="_blank">{titre du lien}</a>
-            <p>Post créé par {{ $post->user->name }}</p>
-            <p>Post présent sur les tableaux :</p>
-            <ul>
-                @foreach ($post->tableaux as $tab)
-                    <li>{{ $tab->nom }}</li>
-                @endforeach
-            </ul>
-            <p>Likes : {{ $post->likes->count() }}</p>
-            <p>Reposts : {{ $post->tableaux->count()-1 }}</p>
-            <p>Supprimer le post</p>
-                <form action="{{ route('post.destroy', $post) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit">X</button>
-                </form>
-        </div>
+        @can('view', $post)
+            <div style="margin: 50px">
+                <a href="{{ $post->url }}" target="_blank">{titre du lien}</a>
+                <p>Post créé par {{ $post->user->name }}</p>
+                <p>Post présent sur les tableaux :</p>
+                <ul>
+                    @foreach ($post->tableaux as $tab)
+                        <li>{{ $tab->nom }}</li>
+                    @endforeach
+                </ul>
+                <p>Likes : {{ $post->likes->count() }}</p>
+                <p>Reposts : {{ $post->tableaux->count()-1 }}</p>
+                <p>Supprimer le post</p>
+                    <form action="{{ route('post.destroy', $post) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit">X</button>
+                    </form>
+            </div>
+        @endcan
     @endforeach
 </body>
 </html>
