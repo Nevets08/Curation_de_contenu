791e2bb4aaafc9526854b5cf0ab0c981928bf932
Steven Robert <54746043+Nevets08@users.noreply.github.com>
2021-02-21 20:57:18 +0100

    Modal liste membres

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,18 @@
-require('./bootstrap');
+require("./bootstrap");
 
-require('alpinejs');
+require("alpinejs");
 
+const membersButton = document.querySelector("aside .members button");
+const modal = document.querySelector(".background-modal");
+
+membersButton.addEventListener("click", () => {
+    modal.classList.add("active-modal");
+    modal.classList.remove("inactive-modal");
+});
+
+const closeModal = document.getElementById("close-modal-button");
+
+closeModal.addEventListener("click", () => {
+    modal.classList.remove("active-modal");
+    modal.classList.add("inactive-modal");
+});
