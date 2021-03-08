779633bd01ea3fb20df6709e1f8a4b1dba3090b6
Johana <jojob.1234@sfr.fr>
2021-03-08 10:52:49 +0100

    Page d'accueil La liste des tableaux ne fonctionne pas Les liens non plus

--- caracara/resources/views/home.blade.php
+++ caracara/resources/views/home.blade.php
@@ -3,42 +3,21 @@
         <section>
             <h2>Tableau privés</h2>
             <div class="tableaux_miniatures">
-                <div>
-                    <a href="{{ route("private_posts") }}">
-                        <img src="{{ asset('img/rectangle_vide.png') }}" alt="">
-                        <p>Lorem Ipsum</p>
-                    </a>
-                </div>
-                <div>
-                    <a href="{{ route("private_posts") }}">
-                        <img src="{{ asset('img/rectangle_vide.png') }}" alt="">
-                        <p>Lorem Ipsum</p>
-                    </a>
-                </div>
-                <div>
-                    <a href="{{ route("private_posts") }}">
-                        <img src="{{ asset('img/rectangle_vide.png') }}" alt="">
-                        <p>Lorem Ipsum</p>
-                    </a>
-                </div>
-                <div>
-                    <a href="{{ route("private_posts") }}">
-                        <img src="{{ asset('img/rectangle_vide.png') }}" alt="">
-                        <p>Lorem Ipsum</p>
-                    </a>
-                </div>
-                <div>
-                    <a href="{{ route("private_posts") }}">
-                        <img src="{{ asset('img/rectangle_vide.png') }}" alt="">
-                        <p>Lorem Ipsum</p>
-                    </a>
-                </div>
-                <div>
-                    <a href="{{ route("private_posts") }}">
-                        <img src="{{ asset('img/rectangle_vide.png') }}" alt="">
-                        <p>Lorem Ipsum</p>
-                    </a>
-                </div>
+                @foreach ($tableaux as $tableau)
+                    @can('view', $tableau){{--Si on a bien accès au tableau--}}
+                        <div>
+                            <a href="{{ route('private_posts') }}">
+                                <img src="
+                                    @if ($tableau->url_icone)
+                                        {{ $tableau->url_icone }}
+                                    @else
+                                        {{ asset('img/rectangle_vide.png') }}
+                                    @endif" alt="">
+                                <p>{{$tableau->nom}}</p>
+                            </a>
+                        </div>
+                    @endcan
+                @endforeach
             </div>
             <button><a href="{{ route("add_tableau") }}">Créer</a></button>
         </section>
@@ -55,51 +34,90 @@
         <section>
             <h2>Mes tableaux publics</h2>
             <div class="tableaux_publics">
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
-                <div onclick="window.location = '{{ route("private_posts") }}'"><img src="{{ asset('img/rond_vide.png') }}" alt=""><p>Lorem Ipsum</p></div>
+                @foreach ($tableaux as $tableau)
+                    @can('view', $tableau){{--Si on a bien accès au tableau--}}
+                        <div onclick="window.location = '{{ route("private_posts") }}'"><img src=" @if ($tableau->url_icone)
+                            {{ $tableau->url_icone }}
+                        @else
+                            {{ asset('img/rond_vide.png') }}
+                            @endif" alt=""><p>{{$tableau->nom}}</p></div>
+                    @endcan
+                @endforeach
             </div>
         </section>
         <section>
-            <article class="article-card">
-                <div class="article-infos">
-                    <p class="article-auteur"><a href="#"><img class="article-auteur-image" src="{{ asset('img/Rond.png') }}" alt="">Nom Prénom</a>&nbspdans&nbsp<a href="#">Anglais</a></p>
-                    <p class="article-date">Il y a 1h</p>
-                </div>
-                <p class="article-headline">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor #incididunt ero labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco poriti laboris nisi ut aliquip ex ea commodo consequat.</p>
-                <img class="article-image" src="{{ asset('img/Rectangle.png') }}" alt="">
-                <div class="article-footer">
-                    <div class="article-likes"><a href=""><i style="margin-right: 5px;" class="far fa-heart"></i>605</a></div>
-                    <div class="article-favoris"><a href=""><i style="margin-right: 5px;" class="far fa-bookmark"></i>Ajouter aux favoris</a></div>
-                    <div class="article-Retweet"><a href=""><i style="margin-right: 5px;" class="fas fa-retweet"></i>Retweet</a></div>
-                    <div class="article-partager">
-                        <a class="article-partager" href="#">Partager</a>
-                        <div class="article-partager-liens">
-                            <a href="https://www.facebook.com/sharer/sharer.php?u=URL">Facebook</a>
-                            <a href="https://twitter.com/intent/tweet?text=URL">Twitter</a>
-                            <a href="https://www.linkedin.com/shareArticle?url=URL">Email</a>
+            @foreach ($posts as $post)
+            @can('abonnement', $post)
+            @php
+                $url = $post->url;
+
+                $opts = array('http'=>array('header' => "User-Agent:MyAgent/1.0\r\n"));
+                $context = stream_context_create($opts);
+
+                preg_match("/<title>(.+)<\/title>/siU", file_get_contents($url, false, $context), $articleTitle);
+                preg_match('/<meta property="og:description" content="(.+)"\/>/siU', file_get_contents($url, false, $context), $articleDescription);
+                preg_match('/<meta property="og:image" content="(.+)"\/>/siU', file_get_contents($url, false, $context), $articleImage);
+
+                $title = $articleTitle[1];
+                $description = isset($articleDescription[1]) ? $articleDescription[1] : null;
+                $image = isset($articleImage[1]) ? $articleImage[1] : null;
+            @endphp
+                <article class="article-card">
+                    <div class="article-infos">
+                        <p class="article-auteur"><a href="#"><img class="article-auteur-image" src="{{ $post->user->profile_photo_url }}" alt="">{{$post->user->name}}</a>&nbspdans&nbsp<a href="#">{{$post->tableaux[0]->nom}}</a></p>
+                        <p class="article-date">Il y a 1h</p>
+                    </div>
+                    <h2><a href="{{$post->url}}">{{$title}}</a></h2>
+                    <p class="article-headline">{{$description}}</p>
+                    <img class="article-image" src="{{ $image }}" alt="">
+                    <div class="article-footer">
+                        <div class="article-likes">
+                            {{-- likes --}}
+                            @php
+                                $user = Auth::user();
+                                $userHasLiked = false;
+                                foreach ($post->likes as $item){
+                                    if (isset($item->id) && $item->id === $user->id)
+                                        $userHasLiked = true;
+                                }
+                            @endphp
+                            @if (!$userHasLiked)
+                                <form action="{{ route('post.update', $post) }}" method="post">
+                                    @csrf
+                                    @method('PUT')
+                                    <input type="hidden" name="user" value="{{$user->id}}">
+                                    <input type="hidden" name="like" value="1">
+
+                                    <button type="submit"><i style="margin-right: 5px;" class="far fa-heart"></i>  {{count($post->likes)}}</button>
+                                </form>
+                            @else
+                                <form action="{{ route('post.update', $post) }}" method="post">
+                                    @csrf
+                                    @method('PUT')
+                                    <input type="hidden" name="user" value="{{$user->id}}">
+                                    <input type="hidden" name="like" value="0">
+
+                                    <button type="submit"><i style="margin-right: 5px;" class="fas fa-heart"></i>{{count($post->likes)}}</button>
+                                </form>
+                            @endif
+                            <span></span>
+                        </div>
+                        {{-- <a href=""><i style="margin-right: 5px;" class="far fa-heart"></i>605</a> --}}
+                        {{-- <div class="article-favoris"><a href=""><i style="margin-right: 5px;" class="far fa-bookmark"></i>Ajouter aux favoris</a></div> --}}
+                        <div class="article-Retweet"><a href=""><i style="margin-right: 5px;" class="fas fa-retweet"></i>Retweet</a></div>
+                        <div class="article-partager">
+                            <a class="article-partager" href="#">Partager</a>
+                            <div class="article-partager-liens">
+                                <a href="https://www.facebook.com/sharer/sharer.php?u={{$post->url}}">Facebook</a>
+                                <a href="https://twitter.com/intent/tweet?text={{$post->url}}">Twitter</a>
+                                <a href="https://www.linkedin.com/shareArticle?url={{$post->url}}">LinkedIn</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </article>
+                </article>
+            @endcan
+            @endforeach
+            
         </section>
 
     </main>
