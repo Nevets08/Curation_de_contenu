dd6d356116ee215dbd930c46b54968681abfc7ab
CamilleChrist <christ.camille@orange.fr>
2021-03-09 20:39:48 +0100

    Ajout route

--- caracara/resources/views/tableaux/all_public_tableaux.blade.php
+++ caracara/resources/views/tableaux/all_public_tableaux.blade.php
@@ -19,7 +19,7 @@
 
         <div class="tableaux">
             @foreach ($tableaux as $tableau)
-                <div>
+                <div  onclick="window.location = '{{ route("tableau.show", $tableau) }}'">
                     <img src="
                         @if ($tableau->url_icone)
                             {{$tableau->url_icone}}
