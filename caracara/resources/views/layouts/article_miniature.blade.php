451037379c3f5316e5c117f8bc3d94b61a77dbcf
Johana <jojob.1234@sfr.fr>
2021-03-10 12:29:29 +0100

    layout articles miniatures + derniers posts user

---
+++ caracara/resources/views/layouts/article_miniature.blade.php
@@ -0,0 +1,20 @@
+@php
+    //Recuperer titre, description et image d'un article
+    $public = public_path('php/OpenGraph.php');
+
+    require_once($public);
+    $url = $post->url;
+    $graph = OpenGraph::fetch($url);
+    foreach ($graph as $key => $value) {
+        if ($key === "title")       {$title = $value;}
+        if ($key === "description") {$description = $value;}
+        if ($key === "image")       {$image = $value;}
+    }
+
+@endphp
+
+<article class="article-miniature">
+    <img src="@if(isset($image)){{ $image }} @else{{ asset('img/carre_vide.png') }}@endif" alt="{{$title}}">
+    <p class="article-miniature-headline"><a href="{{$post->url}}" target="_blank">@isset($title){{ $title }} @endisset</a></p>
+    <p class="article-miniature-infos">Par <a href="#">{{$post->user->name}}</a> dans <a href="#">{{$post->tableaux[0]->nom}}</a></p>
+</article>
