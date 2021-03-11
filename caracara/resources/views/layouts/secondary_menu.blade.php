651a99a8b39fb9d1af55e676b83c8c876c8d8337
Johana <jojob.1234@sfr.fr>
2021-03-11 16:34:51 +0100

    Correction publications sauvegardées quand on en a pas

--- caracara/resources/views/layouts/secondary_menu.blade.php
+++ caracara/resources/views/layouts/secondary_menu.blade.php
@@ -2,5 +2,7 @@
     <li><a href="{{ route('home') }}">Retourner à l'accueil</a></li>
     <li><a href="{{ route('all_private_tableaux') }}">Tableaux privés</a></li>
     <li><a href="{{ route('all_public_tableaux') }}">Tableaux publics</a></li>
+    @if(isset(Auth::user()->tableauSaved))
     <li><a href="{{ route('saved_posts', ['tabID' => Auth::user()->tableauSaved->id]) }}">Vos publications sauvegardées</a></li>
+    @endif
 </ul>
