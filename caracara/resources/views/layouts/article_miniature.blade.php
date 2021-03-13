8d79f8dbe67b4e2fb593d57050ce1f4e3dab9601
Johana <jojob.1234@sfr.fr>
2021-03-13 15:11:12 +0100

    liens vers tableaux

--- caracara/resources/views/layouts/article_miniature.blade.php
+++ caracara/resources/views/layouts/article_miniature.blade.php
@@ -26,5 +26,5 @@
 <article class="article-miniature">
     <img src="@if(isset($image)){{ $image }} @else{{ asset('img/carre_vide.png') }}@endif" alt="{{$title}}">
     <p class="article-miniature-headline"><a href="{{$post->url}}" target="_blank">@isset($title){{ $title }} @endisset</a></p>
-    <p class="article-miniature-infos">Dans <a href="#">{{$post->tableaux[0]->nom}}</a></p>
+    <p class="article-miniature-infos">Dans <a href="{{ route("tableau.show", $post->tableaux[0]) }}">{{$post->tableaux[0]->nom}}</a></p>
 </article>
--- caracara/resources/views/layouts/display_one_post.blade.php
+++ caracara/resources/views/layouts/display_one_post.blade.php
@@ -35,7 +35,7 @@ $date = format_interval($difference);
     <div class="article-infos">
         <p class="article-auteur"><a href="#"><img class="article-auteur-image"
                     src="{{ $post->user->profile_photo_url }}" alt="">{{ $post->user->name }}</a>&nbspdans&nbsp<a
-                href="#">{{ $post->tableaux[0]->nom }}</a></p>
+                href="{{ route("tableau.show", $post->tableaux[0]) }}">{{ $post->tableaux[0]->nom }}</a></p>
         <p class="article-date">{{ $date }}</p>
     </div>
     <h2>{{ $title }}</h2>
