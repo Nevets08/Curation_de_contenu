e8d7fe67a2dc914252447811a63f38a0de4a8b4a
CamilleChrist <christ.camille@orange.fr>
2021-03-14 15:42:34 +0100

    Quelques modifs v5

--- caracara/resources/views/layouts/app.blade.php
+++ caracara/resources/views/layouts/app.blade.php
@@ -52,6 +52,14 @@
                         <li>
                             <hr>
                         </li>
+                        <li><a href="{{ route('all_private_tableaux') }}">Tableaux privés</a></li>
+                        <li><a href="{{ route('all_public_tableaux') }}">Tableaux publics</a></li>
+                        @if(isset(Auth::user()->tableauSaved))
+                            <li><a href="{{ route('saved_posts', ['tabID' => Auth::user()->tableauSaved->id]) }}">Vos publications sauvegardées</a></li>
+                        @endif
+                        <li>
+                            <hr>
+                        </li>
                         <li><a href="{{ route('post.create') }}">Ajouter un article</a></li>
                         <li><a href="{{ route('tableau.create') }}">Créer un tableau</a></li>
                         <li>
